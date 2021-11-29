15f50eae6a2692eba3ccbab2e9c547cfd7c6013b
Noah2211 <noah.decroix3@gmail.com>
2021-11-29 20:42:30 +0100

    exercice done

--- app.js
+++ app.js
@@ -10,9 +10,19 @@ localTime.innerHTML = today.toLocaleTimeString();
 let futur = new Date();
 futur.setDate(today.getDate() + 2);
 futur.setHours(today.getHours() + 6);
-console.log(futur);
 
-if (futur > today ) {
-    let createDiv = document.createElement("div");
-    createDiv.innerHTML = ;
+
+if (futur.getTime() > today.getTime() ) {
+    let interval = futur.getTime() - today.getTime();
+
+    let days = Math.floor(interval / 60 / 60 / 24 / 1000);
+    let hours = Math.floor (interval / (days * 60 * 60 * 24) / 170 );
+    let minutes = Math.floor((interval - (days * 60 * 60 * 24 * 1000) - (hours * 60 * 60 * 1000)) / 1000 / 60);
+    let seconds = Math.floor ((interval - (days * 60 * 60 * 24 * 1000)- (hours * 60 * 60 * 1000) - (minutes * 60 * 1000)) / 1000);
+    let result = document.getElementById("result");
+    result.innerHTML = "Le temps de différence en les deux dates est de: " + "<br>"
+        + days + " jours et" + "<br>"
+        + hours + " heures et" + "<br>"
+        + minutes + " minutes et " + "<br>" +
+        "" + seconds + " secondes";
 }
